0e952dbe3dd197045491514ba81a7186a9858a83
ahmadsky007 <s25960@pjwstk.edu.pl>
2024-05-15 21:34:17 +0200

    Modification 1

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -1,11 +1,12 @@
-﻿
+﻿using System;
+
 namespace MyConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! The current time is " + DateTime.Now);
         }
     }
 }
